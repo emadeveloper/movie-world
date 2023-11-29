5600db3f0739ea08735885153ea0b1676768303e
Emanuel Martinez <yinproducciones@gmail.com>
2023-11-29 18:00:54 -0300

    adding sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { AuthContextProvider } from "./components/context/AuthContext";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Account from "./pages/Account";
 
-function App () {
+function App() {
   return (
     <>
+      <AuthContextProvider>
         <Navbar />
         <Routes>
-            <Route path='/' element={<Home />} />
-            {/* <Route path='/signin' element={<Signin />} />
-            <Route path='/signup' element={<Signup />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/account' element={<Account />} />
         </Routes>
+      </AuthContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
